4fa53e8acff90e175064d9bb671244f7a90e3cb3
Matheus Lima <matheuscl_165@hotmail.com>
2022-05-18 20:25:11 -0300

    resolução exercicios 1

---
+++ 01-primeiros-passos/6-constantes-variaveis-e-constantes-aula-01-06/script.js
@@ -0,0 +1,28 @@
+// age recebe 31
+let age = 31
+let currentYear = 2019
+console.log(age, currentYear)
+
+const points = 1000
+// points = 2000 // Vai gerar um erro, porque constante não pode alterar o valor
+console.log(points)
+
+var score = 75
+console.log(score)
+
+// Regras para nomear uma variável:
+// use CammonCase: nomeVariavel
+// só pode conter letras, números, _ ou $
+// não use palavras chave reservadas: let let, ou let export e etc...
+// coloque nomes claros nas variáveis, para que outros desenvolvedores entendam melhor e mais rápido.
+
+// comentário com uma linha
+/*
+  comentário com várias linhas
+*/
+
+/**
+ * Mais linhas
+ * com 
+ * asterísticos
+ */
